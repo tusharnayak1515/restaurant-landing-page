deb82fbd0360bde38b6dd0b5b7a9ad012787c008
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2023-06-29 18:35:10 +0530

    completed

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const myName = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const contactForm = document.getElementById("contact");
+const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (
+    myName.value.replace("/s/", "").trim().length !== 0 &&
+    emailRegex.test(email.value) &&
+    message.value.replace("/s/", "").trim().length !== 0
+  ) {
+    contactForm.setAttribute("data-bs-toggle", "modal");
+    contactForm.setAttribute("data-bs-target", "#contactModal");
+  }
+});
